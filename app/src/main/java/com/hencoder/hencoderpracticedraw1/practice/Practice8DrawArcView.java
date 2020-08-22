bd8c8032189175371ebd2ef3c07771aef1a4440a
HuangLong <hbxglong@163.com>
2020-08-22 18:57:09 +0800

    增加练习代码

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -1,12 +1,17 @@
 package com.hencoder.hencoderpracticedraw1.practice;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice8DrawArcView extends View {
+
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public Practice8DrawArcView(Context context) {
         super(context);
@@ -20,10 +25,17 @@ public class Practice8DrawArcView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    @SuppressLint("NewApi")
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(getRight()/4,getBottom()/4,getRight()*3/4, getBottom()*3/4,-120,100,true,paint);
+        canvas.drawArc(getRight()/4,getBottom()/4,getRight()*3/4, getBottom()*3/4,30,120,false,paint);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(getRight()/4,getBottom()/4,getRight()*3/4, getBottom()*3/4,180,30,false,paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,12 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
-
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Path path = new Path();
     public Practice9DrawPathView(Context context) {
         super(context);
     }
@@ -25,5 +29,14 @@ public class Practice9DrawPathView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+        paint.setColor(Color.RED);
+//        path.addArc(getRight()/3,getBottom()/4,,);
+
+        path.addCircle(getRight()/3,getBottom()/2,getRight()/6,Path.Direction.CW);
+        path.addCircle(getRight()*2/3,getBottom()/2,getRight()/6,Path.Direction.CW);
+        paint.setStyle(Paint.Style.FILL);
+        path.lineTo(getRight()/2,getBottom());
+        path.rLineTo(-getRight()/3,-getBottom()/2);
+        canvas.drawPath(path,paint);
     }
 }
